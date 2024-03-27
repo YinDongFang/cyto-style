bade5a84e84ae998fc91273c155fcb297a6a964e
Yin <doyin@deloitte.com.cn>
2024-03-27 16:43:28 +0800

    initial version

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+const getMaxValue = require('./getMaxValue')
+const isEvenNumber = require('./isEvenNumber')
+const toLowerCase = require('./toLowerCase')
+const generateUUID = require('./generateUUID')
+
 module.exports = () => {
-  return {
-    name: "style",
-    implement: (cyto) => {},
-  };
-};
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
